1d220e0a34d8a5aede07e997e53cb851b42d3a56
thesaabster <thesaabster@gmail.com>
2017-04-07 16:49:25 +0000

    updated with connectship temporary proxy

--- AX7Connector/Controllers/AxController.cs
+++ AX7Connector/Controllers/AxController.cs
@@ -11,174 +11,124 @@ using System.Web.Http;
 using AX7Connector.Models;
 using AX7Connector.Utilities;
 using System.IO;
-using AX7Connector.Microsoft.Dynamics.DataEntities;
-using Microsoft.OData.Client;
+using System.Diagnostics;
+
 namespace AX7Connector.Controllers
 {
     public class AxController : ApiController
     {
+        static Dictionary<Guid, int> StatusTable = new Dictionary<Guid, int>();
+        static Dictionary<Guid, string> RequestXmlTable = new Dictionary<Guid, string>();
+        static Dictionary<Guid, string> ResponseXmlTable = new Dictionary<Guid, string>();
 
 
-        [HttpPost]
-        [Route("dixf/import")]
-        public async Task<string> Import([FromBody] DIXFEntity entity)
+        [HttpGet]
+        [Route("connectship/getkey")]
+        public async Task<HttpResponseMessage> GetKey()
         {
             HttpResponseMessage responseMsg = Request.CreateResponse(HttpStatusCode.OK);
 
-            //string relativeUri = string.Format(@"{0}", executionId);
-            //responseMsg.Headers.Location = new Uri(Request.RequestUri, relativeUri);
+            Guid key = StatusTable.Where(x => x.Value == 0).FirstOrDefault().Key;
 
-            if (entity == null)
-                return string.Empty;
-
-            /*uri = new Uri("https://dev-matt-3devaos.cloudax.dynamics.com/data");
-
-            var context = new Resources(uri);
-            context.SendingRequest2 += new EventHandler<SendingRequest2EventArgs>(delegate (object sender, SendingRequest2EventArgs e)
+            if (key != Guid.Empty)
             {
-                e.RequestMessage.SetHeader("Authorization", AXUtilities.AuthorizationHeader);
-            });
-
-            var myendpoint = from EndPoint in context.IntegrationDataProjectEndpoints
-                             where EndPoint.LegacySourceEndpointId == entity.SourceEndpointId
-                             select EndPoint;
-            */
-
-            string entityName = "Customer groups";// myendpoint.First().EntityName;
-
-            AXUtilities axUtil = new AXUtilities();
-
-            UriBuilder enqueueUri = new UriBuilder(ClientConfiguration.Default.ActiveDirectoryResource);
-            string activityId = "bd2578ac-9690-4b93-908e-ccfb3b786f42";//myendpoint.First().ActivityId.ToString();
-
-
-            enqueueUri.Path = string.Format(@"api/connector/enqueue/{0}", activityId);
-
-            string enqueueQuery = string.Format("entity={0}", entityName);
-
-            string company = "wcf";//myendpoint.First().Company
-
-            if (!string.IsNullOrEmpty(company))
-            {
-                enqueueQuery += "&company=" + company;
+                StatusTable[key] = 1;
+                responseMsg.Content = new StringContent(RequestXmlTable[key]);
             }
 
-
-            enqueueUri.Query = enqueueQuery;
-
-            HttpResponseMessage response = null;
-
-            using (Stream bodyStream = GenerateStreamFromString(entity.FileContent))
-            {
-                response = axUtil.SendPostRequest(enqueueUri.Uri, AXUtilities.AuthorizationHeader, bodyStream, null, null);
-            }
-            var messageId = await response.Content.ReadAsStringAsync();
-
-
-            return messageId;
+            return responseMsg;
         }
 
-        [HttpGet]
-        [Route("dixf/GetJobStatus/{messageId}")]
-        public async Task<DataJobStatusDetail> GetJobStatus(string messageId)
+        [HttpPost]        
+        [Route("connectship/request")]
+        public async Task<HttpResponseMessage> SendRequest()
         {
-            DataJobStatusDetail jobStatusDetail = null;
-
-            /// get status
-            UriBuilder statusUri = new UriBuilder(ClientConfiguration.Default.ActiveDirectoryResource);
-            string activityId = ClientConfiguration.Default.CustomerImportActivityId;
+            HttpResponseMessage responseMsg = Request.CreateResponse(HttpStatusCode.OK);
 
 
-            statusUri.Path = string.Format(@"api/connector/jobstatus/{0}", activityId);
+            string response = string.Empty;
 
-            string statusQuery = string.Format("jobid={0}", messageId.Replace(@"""", ""));
-
-            statusUri.Query = statusQuery;
-
-
-            //send a request to get the message status
-            AXUtilities axUtil = new AXUtilities();
-
-
-            var response = await axUtil.GetRequestAsync(statusUri.Uri);
-            if (response.IsSuccessStatusCode)
+            try
             {
-                // Deserialize response to the DataJobStatusDetail object
-                jobStatusDetail = JsonConvert.DeserializeObject<DataJobStatusDetail>(response.Content.ReadAsStringAsync().Result);
-            }
-            else
-            {
+                var body = await Request.Content.ReadAsStringAsync();
 
-            }
+                Guid trackerId = Guid.NewGuid();
 
-            return jobStatusDetail;
-        }
+                StatusTable.Add(trackerId, 0);
+                RequestXmlTable.Add(trackerId, body);
 
-        [HttpGet]
-        [Route("dixf/WaitForJob/{messageId}")]
-        public async Task<HttpResponseMessage> WaitForJob(string messageId)
-        {
-            DataJobStatusDetail jobStatusDetail = null;
-            HttpResponseMessage responseMsg = Request.CreateResponse(HttpStatusCode.Accepted);
-            string relativeUri = string.Empty;
-
-
-            /// get status
-            UriBuilder statusUri = new UriBuilder(ClientConfiguration.Default.ActiveDirectoryResource);
-            string activityId = ClientConfiguration.Default.CustomerImportActivityId;
-
-
-            statusUri.Path = string.Format(@"api/connector/jobstatus/{0}", activityId);
-
-            string statusQuery = string.Format("jobid={0}", messageId.Replace(@"""", ""));
-
-            statusUri.Query = statusQuery;
-
-
-            //send a request to get the message status
-            AXUtilities axUtil = new AXUtilities();
-
-
-            var response = await axUtil.GetRequestAsync(statusUri.Uri);
-            if (response.IsSuccessStatusCode)
-            {
-                // Deserialize response to the DataJobStatusDetail object
-                jobStatusDetail = JsonConvert.DeserializeObject<DataJobStatusDetail>(response.Content.ReadAsStringAsync().Result);
-
-                if (jobStatusDetail != null)
+                Stopwatch watch = new Stopwatch();
+                watch.Start();
+                while (watch.Elapsed.Seconds <= 30)
                 {
-                    if (jobStatusDetail.DataJobStatus.DataJobState == DataJobState.Processed || jobStatusDetail.DataJobStatus.DataJobState == DataJobState.ProcessedWithErrors)
-                    {
-                        responseMsg.StatusCode = HttpStatusCode.OK;
-                    }
-                    else
-                    {
-                        responseMsg.StatusCode = HttpStatusCode.Accepted;
-                        relativeUri = string.Format("{0}", messageId);
-                        responseMsg.Headers.Location = new Uri(Request.RequestUri, relativeUri);
-                    }
+                    ResponseXmlTable.TryGetValue(trackerId, out response);
+
+                    if (!string.IsNullOrEmpty(response))
+                        break;
+
 
                 }
-            }
-            else
-            {
+
+                if (string.IsNullOrEmpty(response))
+                {
+                    responseMsg.StatusCode = HttpStatusCode.RequestTimeout;
+
+                }
+
+                responseMsg.Content = new StringContent(response, System.Text.Encoding.UTF8, "application/soap+xml");
 
             }
+            catch (Exception ex)
+            {
+                response = ex.ToString();
+                responseMsg.Content = new StringContent(response);
+            }
+
+
 
 
             return responseMsg;
         }
 
-        private static Stream GenerateStreamFromString(string s)
-        {
-            MemoryStream stream = new MemoryStream();
-            StreamWriter writer = new StreamWriter(stream);
-            writer.Write(s);
-            writer.Flush();
-            stream.Position = 0;
-            return stream;
-        }
 
+
+
+        [HttpPost]
+        [Route("connectship/setresponse")]
+        public async Task<HttpResponseMessage> SetResponse()
+        {
+            HttpResponseMessage responseMsg = Request.CreateResponse(HttpStatusCode.OK);
+            string response = string.Empty;
+
+            try
+            {
+                var body = await Request.Content.ReadAsStringAsync();
+
+                Guid key = StatusTable.Where(x => x.Value == 1).FirstOrDefault().Key;
+
+                if (key != Guid.Empty)
+                {
+                    StatusTable[key] = 2;
+
+
+                    if (!ResponseXmlTable.ContainsKey(key))
+                    {
+                        ResponseXmlTable.Add(key, body);
+                    }
+
+                    responseMsg.Content = new StringContent(Convert.ToString(key));
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                responseMsg.Content = new StringContent(ex.ToString());
+
+            }
+
+            return responseMsg;
+        }
 
     }
 }
